e02289c53357829ff3eacbc41cc09b0491eef890
Hsien-Hsiu Liao <hsien-hsiu.liao@yandex.com>
2020-05-26 17:06:26 -0700

    update components

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # multisig-wallet
 
 A quorum of at least 2 out of 3 people must occur in order for funds to be transferred
+
+
+If having errors running npm, 
+Restore ownership of the user's npm related folders, to the current user, like this:
+
+sudo chown -R $USER:$GROUP ~/.npm
+sudo chown -R $USER:$GROUP ~/.config
+
--- client/src/utils.js
+++ client/src/utils.js
@@ -2,14 +2,39 @@ import Web3 from 'web3';
 import Wallet from './contracts/Wallet.json';
 
 const getWeb3 = () => {
-    return new Web3('http://localhost:9545');
+    console.log('getweb3:');
+    return new Promise((resolve, reject) => {
+        window.addEventListener('load', async () => {
+
+        console.log('windowethereum:', window.ethereum )
+        //if Metamask is present it will inject an ethereum object onto window object
+        if(window.ethereum) {
+            // Web3( provider object ) , http provider like http://localhost
+            const web3 = new Web3(window.ethereum);
+            try {
+                await window.ethereum.enable(); // ask user to enable Metmask for dapp
+                resolve(web3);
+            } catch(error) {
+                reject(error);
+            }
+        } else if(window.web3) { //old version Metamask installed
+            resolve(window.web3);
+        } else {
+            reject('Must install Metamask');
+        }
+    });
+});
+
+    //connect to local development blockchain
+    //special feature that it accepts transactions without being signed
+     //return new Web3('http://localhost:9545');
 };
 
 const getWallet = async web3 => {
     const networkId = await web3.eth.net.getId();
-    console.log("Wallet:", Wallet.networks);
+   // console.log("Wallet:", Wallet.networks);
     const deployedNetwork = Wallet.networks[networkId];
-    console.log(deployedNetwork.address);
+    //console.log(deployedNetwork.address);
     return new web3.eth.Contract(
         Wallet.abi,
         deployedNetwork && deployedNetwork.address
